7fe4454e0a56bcfb34c52fad3cbc231f0ed169fc
Lucas Mançan <lucasfmancan@hotmail.com>
2022-07-09 19:52:12 -0300

    Update pubmed_search.py

--- pubmed_search.py
+++ pubmed_search.py
@@ -19,10 +19,12 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 all_articles = []
 
+total_elements = soup.find(class_="results-amount").find(class_="value").get_text()
 
 pagination = soup.find(class_ = "page-number-wrapper")
 total_pages = soup.find(class_ = "of-total-pages").get_text().replace("of ", "")
-print("Total de elementos encontrados: " + str(int(total_pages) * 200))
+
+print("Total de elementos encontrados: " + total_elements)
 print("Aguarde a execução do script...")
 for page in range(0, int(total_pages)):
     page = requests.get(
@@ -45,7 +47,12 @@ for page in range(0, int(total_pages)):
         all_articles.append([title, doi])
        
 
-file_name = os.path.expanduser("~") + "\Documents\pumed" + "\-" +search_term + "-" + datetime.now().strftime("%d_%m_%Y_%H_%M") + ".csv"
+folderpath = os.path.expanduser("~") + "\Documents\pesquisas_pubmed"
+if not os.path.exists(folderpath):
+    os.makedirs(folderpath)
+    print('Created:', folderpath)
+
+file_name = folderpath + "\-" +search_term + "-" + datetime.now().strftime("%d_%m_%Y_%H_%M") + ".csv"
 for article in all_articles:
      with open(file_name, 'w', newline='',encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
@@ -54,3 +61,5 @@ for article in all_articles:
         writer.writerows(all_articles)
 
 print("Arquivo salvo: "+ file_name)
+
+input("Pressione ENTER para continuar")
